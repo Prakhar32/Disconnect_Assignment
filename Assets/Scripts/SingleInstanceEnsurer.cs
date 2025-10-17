b7f3d4cf60f3d668f21869e092839a59a098e978
Prakhar Singh <prkhrsingh3@gmail.com>
2025-10-17 06:15:10 +0530

    Changed instantiation logic

--- Assets/Scripts/SingleInstanceEnsurer.cs
+++ Assets/Scripts/SingleInstanceEnsurer.cs
@@ -1,12 +1,33 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Samples.ARStarterAssets;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 public class SingleInstanceEnsurer : MonoBehaviour
 {
+    private ObjectSpawner _spawner;
+    private ARInteractorSpawnTrigger _spawnTrigger;
     void Start()
     {
-        if (FindObjectsByType<SingleInstanceEnsurer>(FindObjectsSortMode.None).Length > 1)
+        _spawnTrigger = GetComponent<ARInteractorSpawnTrigger>();
+        _spawner = GetComponent<ObjectSpawner>();
+
+        if( _spawnTrigger == null)
         {
-            Destroy(gameObject);
+            Destroy(this);
+            throw new MissingComponentException("ARInteractorSpawnTrigger missing");
         }
+
+        if (_spawner == null)
+        {
+            Destroy(this);
+            throw new MissingComponentException("ObjectSpawner missing");
+        }
+
+        _spawner.objectSpawned += _spawner_objectSpawned;
+    }
+
+    private void _spawner_objectSpawned(GameObject obj)
+    {
+        _spawnTrigger.enabled = false;
     }
 }
